e5805f35cc3c466fd3e378de657cf499cfed4cb5
MiCHiLU <djmchl@gmail.com>
2013-11-11 23:47:10 +0800

    bump up for dev

--- src/picasa-dl/main.go
+++ src/picasa-dl/main.go
@@ -24,7 +24,7 @@ const (
 
 	TWBSversion               = "3.0.1"
 	defaultUserID             = "sample.user"
-	majorVersion              = "1.1"
+	majorVersion              = "1.2dev"
 	maxGoroutine              = 100
 	permDir       os.FileMode = 0755
 	permFile      os.FileMode = 0644
