f95b9e0d3d282508c7fe203d104a9589be65b8de
MiCHiLU <djmchl@gmail.com>
2013-11-11 22:35:32 +0800

    set charset in html

--- src/picasa-dl/main.go
+++ src/picasa-dl/main.go
@@ -151,6 +151,7 @@ const indexhtml = `<!DOCTYPE html>
 !!! 5
 %html/
 %head
+ %meta(charset="UTF-8")
  %link(href="../bootstrap-%v.min.css" rel="stylesheet")
 %body/
 .row %v
@@ -158,6 +159,7 @@ const indexhtml = `<!DOCTYPE html>
 const html = `<!DOCTYPE html>
 <html>
 <head>
+<meta charset="UTF-8">
 <link href='../bootstrap-%v.min.css' rel='stylesheet'>
 </head>
 <body>
