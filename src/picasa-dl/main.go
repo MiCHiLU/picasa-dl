28f662ac7025506e07a787992182cae9a998ab15
MiCHiLU <djmchl@gmail.com>
2013-11-18 19:32:19 +0800

    release 1.3

--- src/picasa-dl/main.go
+++ src/picasa-dl/main.go
@@ -29,7 +29,7 @@ const (
 
 	TWBSversion               = "3.0.1"
 	defaultUserID             = "sample.user"
-	majorVersion              = "1.3dev"
+	majorVersion              = "1.3"
 	maxGoroutine              = 100
 	permDir       os.FileMode = 0755
 	permFile      os.FileMode = 0644
